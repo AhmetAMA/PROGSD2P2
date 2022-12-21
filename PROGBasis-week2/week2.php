929344852604c76f8bf8676ce9badfc622fad2b6
AhmetAMA <33016@ma-web.nl>
2022-12-21 11:57:10 +0100

    updated werk

--- PROGBasis-week2/week2.php
+++ PROGBasis-week2/week2.php
@@ -6,11 +6,23 @@
  * Voor de key en value gebruik je kleine letters, geen hoofdletters
  */
 
+    $weekdagen = array(
+        'ma' => 'maandag',
+        'di' => 'dinsdag',
+        'wo' => 'woensdag',
+        'do' => 'donderdag',
+        'vr' => 'vrijdag',
+        'za' => 'zaterdag',
+        'zo' => 'zondag'
+    );
+
 /*
  * Opdracht 2:
  * Gebruik een loop om van alle elementen van je array de waarde te tonen op het scherm
  * Gebruik hiervoor een echo statement dus geen print_r of var_dump.
  */
+
+    //  Staat samen bij "Opdracht 3".
 
 /*
  * Opdracht 3:
@@ -20,6 +32,23 @@
  * Gebruik hiervoor de juiste functies
  */
 
+    foreach ($weekdagen as $key => $dag) {
+        switch($key){
+            case 'ma';
+            case 'di';
+            case 'wo';
+            case 'do';
+            case 'vr';
+            echo ucfirst (strtolower (htmlspecialchars($dag)));
+            break;
+            case 'za';
+            case 'zo';
+            echo ucfirst (strtoupper (htmlspecialchars($dag)));
+            default:
+            break;
+        };
+    };
+
 /*
  * Opdracht 4:
  * Maak een nieuwe array en noem deze $omgekeerd.
@@ -28,11 +57,26 @@
  * HINT: de grootte van een array kun je met sizeof() en count() bepalen
  */
 
+    echo '<br>';
+    echo '<br>';
+    echo "dit is count() in een foreach loop door een key en waarde";
+    foreach($weekdagen as $dagen => $dag){
+        echo "$dagen:  $dag";
+    };
+
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "dit is sizeof() in een for loop";
+    echo "<br>";
+    $size = sizeof($weekdagen);
+    for($i = 6; $i < $size; ++$i){
+        echo $size;
+    };
+
 /*
  * Opdracht 5:
  * Gebruik de functie array_reverse om hetzelfde resultaat als in opdracht 4 te verkrijgen.
  */
 
-$weekdagen = array(
-    'ma' => 'maandag'
-    );
+    
